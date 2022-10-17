233d98e58b81f3cb7fcaee5af46c1875331a3be6
SergiuPs <sergiu.pislaru@web.de>
2022-10-17 13:00:18 +0200

    BaseEntity added

---
+++ book-library-data/src/main/java/de/pislaru/sergiu/booklibrary/model/BaseEntity.java
@@ -0,0 +1,120 @@
+package de.pislaru.sergiu.booklibrary.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Version
+    private Long version;
+
+    private String createdBy;
+    private String updatedBy;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void setCreationDate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void setChangeDate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || object.getClass() != this.getClass()) {
+            return false;
+        }
+
+        BaseEntity baseEntity = (BaseEntity) object;
+
+        return Objects.equals(id, baseEntity.getId()) &&
+                Objects.equals(version, baseEntity.version) &&
+                Objects.equals(createdBy, baseEntity.createdBy) &&
+                Objects.equals(createdAt, baseEntity.createdAt) &&
+                Objects.equals(updatedBy, baseEntity.updatedBy) &&
+                Objects.equals(updatedAt, baseEntity.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, version, createdBy, createdAt, updatedBy, updatedAt);
+    }
+
+    @Override
+    public String toString() {
+        return "Base Entity { "
+                + "id=" + id
+                + ", version=" + version
+                + ", created-by=" + createdBy
+                + ", created-at=" + createdAt
+                + ", updated-by=" + updatedBy
+                + ", updated-at=" + updatedAt
+                + "}";
+    }
+}
