d4258ad387e625202ed5191669d91aed3dc3471d
SergiuPs <sergiu.pislaru@web.de>
2022-10-17 15:09:07 +0200

    Add Region enumeration

---
+++ book-library-data/src/main/java/de/pislaru/sergiu/booklibrary/model/Region.java
@@ -0,0 +1,31 @@
+package de.pislaru.sergiu.booklibrary.model;
+
+public enum Region {
+
+    BW("Baden-Württemberg"),
+    BY("Bayern"),
+    BE("Berlin"),
+    BB("Brandenburg"),
+    HB("Bremen"),
+    HH("Hamburg"),
+    HE("Hessen"),
+    MV("Mecklenburg-Vorpommern"),
+    NI("Niedersachsen"),
+    NW("Nordrhein-Westfalen"),
+    RP("Rheinland-Pfalz"),
+    SL("Saarland"),
+    SN("Sachsen"),
+    ST("Sachsen-Anhalt"),
+    SH("Schleswig-Holstein"),
+    TH("Thüringen");
+
+    private String name;
+
+    Region(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
