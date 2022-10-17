95cf92827067d70b676afbea58246d5de41c64fc
SergiuPs <sergiu.pislaru@web.de>
2022-10-17 17:17:48 +0200

    Add City entity

---
+++ book-library-data/src/main/java/de/pislaru/sergiu/booklibrary/model/City.java
@@ -0,0 +1,65 @@
+package de.pislaru.sergiu.booklibrary.model;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class City extends BaseEntity implements Serializable {
+
+    private String name;
+
+    @Enumerated(value = EnumType.STRING)
+    private Region region;
+
+    public City() {
+    }
+
+    public City(String name, Region region) {
+        this.name = name;
+        this.region = region;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Region getRegion() {
+        return region;
+    }
+
+    public void setRegion(Region region) {
+        this.region = region;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || this.getClass() != object.getClass() || !super.equals(object)) {
+            return false;
+        }
+
+        City city = (City) object;
+
+        return Objects.equals(name, city.name) &&
+                Objects.equals(region, city.region);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), name, region);
+    }
+
+    @Override
+    public String toString() {
+        return "City {"
+                + "name=" + name
+                + ", region=" + region.getName()
+                + "}"
+                + super.toString();
+    }
+}
