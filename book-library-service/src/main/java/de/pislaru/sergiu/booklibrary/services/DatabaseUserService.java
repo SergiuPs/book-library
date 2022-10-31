a4f9bf05e428eb1bb81574bb9e4b7da1de1d20f0
SergiuPs <sergiu.pislaru@web.de>
2022-10-31 21:58:41 +0100

    Refactored account service to user service and implemented method to get all users that have role less or equal than specified level

--- book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/services/DatabaseUserService.java
+++ book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/services/DatabaseUserService.java
@@ -6,17 +6,21 @@ import de.pislaru.sergiu.booklibrary.model.UserInfo;
 import de.pislaru.sergiu.booklibrary.repositories.RoleRepository;
 import de.pislaru.sergiu.booklibrary.repositories.UserRepository;
 
+import de.pislaru.sergiu.booklibrary.utils.ModelMapperUtils;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Service
-public class DatabaseAccountService implements AccountService{
+public class DatabaseUserService implements UserService {
 
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
     private final RoleRepository roleRepository;
 
-    public DatabaseAccountService(UserRepository userRepository, ModelMapper modelMapper, RoleRepository roleRepository) {
+    public DatabaseUserService(UserRepository userRepository, ModelMapper modelMapper, RoleRepository roleRepository) {
         this.userRepository = userRepository;
         this.modelMapper = modelMapper;
         this.roleRepository = roleRepository;
@@ -28,7 +32,7 @@ public class DatabaseAccountService implements AccountService{
         User user = modelMapper.map(userInfo, User.class);
 
         if (user.getRoles().size() == 0) {
-            Role userRole = roleRepository.getFirstByName(Role.ROLE_ADMIN);
+            Role userRole = roleRepository.getFirstByName(Role.ROLE_USER);
             user.addRole(userRole);
         }
 
@@ -48,6 +52,20 @@ public class DatabaseAccountService implements AccountService{
 
     @Override
     public boolean changePassword(Long id, String oldPassword, String newPassword) {
+        return false;
+    }
+
+    @Override
+    public Set<UserInfo> getAllUsersWithRoleLevelEqualOrLessThan(byte level) {
+        Set<User> users = userRepository.getAllByRolesLevelLessThanEqual(level);
+        Set<UserInfo> userInfos = new HashSet<>();
+        ModelMapperUtils.mapSet(users, UserInfo.class, modelMapper);
+
+        return userInfos;
+    }
+
+    @Override
+    public boolean userExists(String usernameOrEmail) {
         return false;
     }
 }
--- book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/services/UserService.java
+++ book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/services/UserService.java
@@ -2,9 +2,13 @@ package de.pislaru.sergiu.booklibrary.services;
 
 import de.pislaru.sergiu.booklibrary.model.UserInfo;
 
-public interface AccountService {
+import java.util.Set;
+
+public interface UserService {
     UserInfo register(UserInfo userInfo);
     UserInfo update(UserInfo userInfo);
     boolean delete(Long id);
     boolean changePassword(Long id, String oldPassword, String newPassword);
+    Set<UserInfo> getAllUsersWithRoleLevelEqualOrLessThan(byte level);
+    boolean userExists(String usernameOrEmail);
 }
