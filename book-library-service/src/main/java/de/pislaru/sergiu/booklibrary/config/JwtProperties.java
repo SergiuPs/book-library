4ffcdfa7965c302716d8aa29c9017c77a2087a1d
SergiuPs <sergiu.pislaru@web.de>
2023-07-09 12:59:24 +0200

    JwtProperties and maven dependency for configuration-processor

---
+++ book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/config/JwtProperties.java
@@ -0,0 +1,17 @@
+package de.pislaru.sergiu.booklibrary.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "book.library")
+public class JwtProperties {
+
+    private String jwtKey;
+    private Long jwtExpiresAfter;
+
+    public String getJwtKey() {return jwtKey;}
+    public void setJwtKey(String jwtKey) {this.jwtKey = jwtKey;}
+    public Long getJwtExpiresAfter() {return jwtExpiresAfter;}
+    public void setJwtExpiresAfter(Long jwtExpiresAfter) {this.jwtExpiresAfter = jwtExpiresAfter;}
+}
