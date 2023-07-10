f90eeb21b632d0dddf447cf2e57c75c00016e695
SergiuPs <sergiu.pislaru@web.de>
2023-07-10 14:00:13 +0200

    Constants Class for RestApi

---
+++ book-library-web/src/main/java/de/pislaru/sergiu/booklibrary/constants/RestApiConstants.java
@@ -0,0 +1,10 @@
+package de.pislaru.sergiu.booklibrary.constants;
+
+public final class RestApiConstants {
+
+    public static final String API_PREFIX = "/api";
+    public static final String CURRENT_VERSION = API_PREFIX + "/v1";
+
+    private RestApiConstants() {
+    }
+}
