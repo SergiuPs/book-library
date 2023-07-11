991f409038d8f403fe56653fca2fc7751d3850f0
SergiuPs <sergiu.pislaru@web.de>
2023-07-11 13:04:27 +0200

    Added Email constants and renamed the template

---
+++ book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/constants/EmailConstants.java
@@ -0,0 +1,11 @@
+package de.pislaru.sergiu.booklibrary.constants;
+
+public final class EmailConstants {
+    public final static String BASE_PACKAGE_PATH = "/templates";
+    public final static String CONFIRM_REGISTRATION_PATH = "/confirm-registration?token=";
+
+    public final static String EMAIL_CONFIRMATION_LINK_TEMPLATE = "registration-confirmation-email.flth";
+
+    private EmailConstants(){
+    }
+}
