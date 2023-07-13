86161733dbfd00f48b9d89636084305653925d76
SergiuPs <sergiu.pislaru@web.de>
2023-07-13 18:44:42 +0200

    Email package, moved Constants

--- book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/email/EmailConstants.java
+++ book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/email/EmailConstants.java
@@ -1,10 +1,11 @@
-package de.pislaru.sergiu.booklibrary.constants;
+package de.pislaru.sergiu.booklibrary.services.email;
 
 public final class EmailConstants {
     public final static String BASE_PACKAGE_PATH = "/templates";
     public final static String CONFIRM_REGISTRATION_PATH = "/confirm-registration?token=";
 
     public final static String EMAIL_CONFIRMATION_LINK_TEMPLATE = "registration-confirmation-email.flth";
+    public final static String EMAIL_SECURITY_CODE_TEMPLATE = "security-code-email.flth";
 
     private EmailConstants(){
     }
