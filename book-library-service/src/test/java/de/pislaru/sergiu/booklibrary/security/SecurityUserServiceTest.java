7c5db76f169431e903faab815a44aeff4ccbc095
SergiuPs <sergiu.pislaru@web.de>
2023-07-15 11:37:50 +0200

    Renamed tests

--- book-library-service/src/test/java/de/pislaru/sergiu/booklibrary/security/SecurityUserServiceTest.java
+++ book-library-service/src/test/java/de/pislaru/sergiu/booklibrary/security/SecurityUserServiceTest.java
@@ -1,8 +1,7 @@
 package de.pislaru.sergiu.booklibrary.security;
 
-import de.pislaru.sergiu.booklibrary.model.User;
-import de.pislaru.sergiu.booklibrary.model.UserInfo;
-import de.pislaru.sergiu.booklibrary.model.security.SecurityUser;
+import de.pislaru.sergiu.booklibrary.model.user.User;
+import de.pislaru.sergiu.booklibrary.user.dto.UserDTO;
 import de.pislaru.sergiu.booklibrary.repositories.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,7 +13,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -22,7 +23,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class DatabaseUserDetailsServiceTest {
+class SecurityUserServiceTest {
 
     @Mock
     UserRepository userRepository;
@@ -31,35 +32,36 @@ class DatabaseUserDetailsServiceTest {
     ModelMapper modelMapper;
 
     @InjectMocks
-    DatabaseUserDetailsService userDetailsService;
+    SecurityUserService userDetailsService;
 
-    User user;
-    UserInfo userInfo;
+    Optional<User> optionalUser;
+    UserDTO userDTO;
     SecurityUser securityUser;
 
     @BeforeEach
     void setUp() {
-        user = new User("Max", "Mustermann", "Maxim", "max@web.de", "pass",
+        User user = new User("Max", "Mustermann", "Maxim", "max@web.de", "pass", true,
                 new HashSet<>(), new HashSet<>());
         user.setId(1L);
+        optionalUser = Optional.of(user);
 
-        userInfo = new UserInfo("Max", "Mustermann", "Maxim", "max@web.de", "pass",
+        userDTO = new UserDTO("Max", "Mustermann", "Maxim", "max@web.de", "pass",
                 new HashSet<>(), new HashSet<>());
-        userInfo.setId(1L);
+        userDTO.setId(1L);
 
-        securityUser = new SecurityUser(userInfo);
+        securityUser = new SecurityUser(user, new ArrayList<>());
     }
 
     @Test
     void loadUserByUsername() {
 
-        when(userRepository.getFirstByUserName(any())).thenReturn(user);
-        when(modelMapper.map(user, UserInfo.class)).thenReturn(userInfo);
+        when(userRepository.findByUserName(any())).thenReturn(optionalUser);
+        when(modelMapper.map(optionalUser, UserDTO.class)).thenReturn(userDTO);
 
         UserDetails userDetails = userDetailsService.loadUserByUsername("Maxim");
 
-        verify(userRepository, times(1)).getFirstByUserName(any());
-        verify(userRepository, times(0)).getFirstByEmail(any());
+        verify(userRepository, times(1)).findByUserName(any());
+        verify(userRepository, times(0)).findByEmail(any());
         assertEquals(userDetails, securityUser);
         assertEquals("Maxim", securityUser.user().getUserName());
     }
@@ -67,14 +69,14 @@ class DatabaseUserDetailsServiceTest {
     @Test
     void loadUserByEmail() {
 
-        when(userRepository.getFirstByUserName(any())).thenReturn(null);
-        when(userRepository.getFirstByEmail(any())).thenReturn(user);
-        when(modelMapper.map(user, UserInfo.class)).thenReturn(userInfo);
+        when(userRepository.findByUserName(any())).thenReturn(null);
+        when(userRepository.findByEmail(any())).thenReturn(optionalUser);
+        when(modelMapper.map(optionalUser, UserDTO.class)).thenReturn(userDTO);
 
         UserDetails userDetails = userDetailsService.loadUserByUsername("max@web.de");
 
-        verify(userRepository, times(1)).getFirstByUserName(any());
-        verify(userRepository, times(1)).getFirstByEmail(any());
+        verify(userRepository, times(1)).findByUserName(any());
+        verify(userRepository, times(1)).findByEmail(any());
         assertEquals(userDetails, securityUser);
         assertEquals("max@web.de", securityUser.user().getEmail());
     }
@@ -82,15 +84,15 @@ class DatabaseUserDetailsServiceTest {
     @Test
     void loadUserByUsernameThrowsException() {
 
-        when(userRepository.getFirstByUserName(any())).thenReturn(null);
-        when(userRepository.getFirstByEmail(any())).thenReturn(null);
+        when(userRepository.findByUserName(any())).thenReturn(null);
+        when(userRepository.findByEmail(any())).thenReturn(null);
 
         assertThrows(UsernameNotFoundException.class, () -> {
             userDetailsService.loadUserByUsername("Unknown");
         });
 
-        verify(userRepository, times(1)).getFirstByUserName(any());
-        verify(userRepository, times(1)).getFirstByEmail(any());
+        verify(userRepository, times(1)).findByUserName(any());
+        verify(userRepository, times(1)).findByEmail(any());
 
     }
 }
