2f610932533ae37a34a6198846e57db843d0c1f7
SergiuPs <sergiu.pislaru@web.de>
2023-07-16 18:47:12 +0200

    User Registration event and listener

---
+++ book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/user/account/events/OnUserRegistrationCompleteEvent.java
@@ -0,0 +1,22 @@
+package de.pislaru.sergiu.booklibrary.user.account.events;
+
+import de.pislaru.sergiu.booklibrary.model.user.User;
+import org.springframework.context.ApplicationEvent;
+
+public class OnUserRegistrationCompleteEvent extends ApplicationEvent {
+
+    private User user;
+    private String applicationUrl;
+
+
+    public OnUserRegistrationCompleteEvent(User user, String applicationUrl) {
+        super(user);
+        this.user = user;
+        this.applicationUrl = applicationUrl;
+    }
+
+    public User getUser() {return user;}
+    public void setUser(User user) {this.user = user;}
+    public String getApplicationUrl() {return applicationUrl;}
+    public void setApplicationUrl(String applicationUrl) {this.applicationUrl = applicationUrl;}
+}
---
+++ book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/user/account/events/UserRegistrationListener.java
@@ -0,0 +1,63 @@
+package de.pislaru.sergiu.booklibrary.user.account.events;
+
+import de.pislaru.sergiu.booklibrary.email.*;
+import de.pislaru.sergiu.booklibrary.email.service.EmailService;
+import de.pislaru.sergiu.booklibrary.model.user.User;
+import de.pislaru.sergiu.booklibrary.user.account.service.VerificationTokenService;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class UserRegistrationListener implements ApplicationListener<OnUserRegistrationCompleteEvent> {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserRegistrationListener.class);
+
+    private final VerificationTokenService tokenService;
+    private final EmailService emailService;
+
+    public UserRegistrationListener(VerificationTokenService tokenService, EmailService emailService) {
+        this.tokenService = tokenService;
+        this.emailService = emailService;
+    }
+
+    @Override
+    public void onApplicationEvent(OnUserRegistrationCompleteEvent event) {
+        User user = event.getUser();
+        String token = tokenService.createVerificationToken(user);
+
+        String confirmationUrl = event.getApplicationUrl() + EmailConstants.CONFIRM_REGISTRATION_PATH + token;
+        Map<String, Object> templateModel = new HashMap<>();
+        templateModel.put("firstName", user.getFirstName());
+        templateModel.put("lastName", user.getLastName());
+        templateModel.put("confirmationLink", confirmationUrl);
+
+        EmailContent emailContent;
+        try {
+            Template template = emailService.getTemplateFromString(EmailConstants.EMAIL_CONFIRMATION_LINK_TEMPLATE);
+            emailContent = new EmailContent(template, templateModel);
+        } catch (IOException | TemplateException e) {
+            logger.error("Loading template failed: {}", e.getMessage());
+            emailContent = new EmailContent(confirmationUrl);
+        }
+
+        Email email = new Email(user.getEmail(),
+                EmailSender.NO_REPLY,
+                EmailSubject.REGISTRATION_CONFIRMATION,
+                emailContent);
+        try {
+            emailService.sendEmail(email);
+        } catch (MessagingException e) {
+            logger.error("Sending email validation failed: {}", e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+}
