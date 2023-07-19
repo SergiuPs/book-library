6e11dbf1d7ed3ccbf85eda96943267c195e6bca2
SergiuPs <sergiu.pislaru@web.de>
2023-07-19 22:01:10 +0200

    AddressRepository

---
+++ book-library-data/src/main/java/de/pislaru/sergiu/booklibrary/repositories/AddressRepository.java
@@ -0,0 +1,13 @@
+package de.pislaru.sergiu.booklibrary.repositories;
+
+import de.pislaru.sergiu.booklibrary.model.address.Address;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+;
+
+@Repository
+public interface AddressRepository extends CrudRepository<Address, Long> {
+    List<Address> findAllByUserIdOrderByIdAsc(Long userId);
+}
