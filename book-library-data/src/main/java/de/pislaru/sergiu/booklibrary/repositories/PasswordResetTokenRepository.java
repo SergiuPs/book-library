e0616a40454d7071fa9c9531ff9692515393b975
SergiuPs <sergiu.pislaru@web.de>
2023-07-24 22:02:24 +0200

    Repository for PasswordResetToken

---
+++ book-library-data/src/main/java/de/pislaru/sergiu/booklibrary/repositories/PasswordResetTokenRepository.java
@@ -0,0 +1,12 @@
+package de.pislaru.sergiu.booklibrary.repositories;
+
+import de.pislaru.sergiu.booklibrary.model.user.PasswordResetToken;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PasswordResetTokenRepository extends CrudRepository<PasswordResetToken, Long> {
+    Optional<PasswordResetToken> findByToken(String token);
+}
