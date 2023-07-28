e0165cf78c4559087e8b59e68f32acffd5cc8775
SergiuPs <sergiu.pislaru@web.de>
2023-07-28 21:53:56 +0200

    Added birthDate and avatar to UerDTO

--- book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/user/dto/UserDTO.java
+++ book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/user/dto/UserDTO.java
@@ -3,6 +3,7 @@ package de.pislaru.sergiu.booklibrary.user.dto;
 import de.pislaru.sergiu.booklibrary.address.AddressDTO;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -12,25 +13,17 @@ public class UserDTO implements Serializable {
 
     private Long id;
     private Long version;
+    private String userName;
+    private String email;
     private String firstName;
     private String lastName;
-    private String email;
-    private String userName;
+    private LocalDate birthDate;
+    private String avatar;
     private String password;
     private List<RoleDTO> roles = new ArrayList<>();
     private List<AddressDTO> addresses = new ArrayList<>();
 
     public UserDTO() {
-    }
-
-    public UserDTO(String firstName, String lastName, String userName, String email, String password, List<RoleDTO> roles, List<AddressDTO> addresses) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.userName = userName;
-        this.password = password;
-        this.roles = roles;
-        this.addresses = addresses;
     }
 
     public boolean equals(Object object) {
@@ -43,10 +36,12 @@ public class UserDTO implements Serializable {
 
         return  Objects.equals(this.id, that.id) &&
                 Objects.equals(this.version, that.version) &&
-                Objects.equals(this.firstName, that.firstName) &&
-                Objects.equals(this.lastName, that.lastName) &&
                 Objects.equals(this.userName, that.userName) &&
                 Objects.equals(this.email, that.email) &&
+                Objects.equals(this.firstName, that.firstName) &&
+                Objects.equals(this.lastName, that.lastName) &&
+                Objects.equals(this.birthDate, that.birthDate) &&
+                Objects.equals(this.avatar, that.avatar) &&
                 Objects.equals(this.addresses, that.addresses) &&
                 Objects.equals(this.roles, that.roles);
     }
@@ -65,6 +60,8 @@ public class UserDTO implements Serializable {
                 + ", last-name=" + lastName
                 + ", e-mail=" + email
                 + ", user-name=" + userName
+                + ", birth-date=" + birthDate
+                + ", avatar=" + avatar
                 + ", addresses: " + addresses.toString()
                 + " roles: " + roles.toString()
                 + "}";
@@ -74,14 +71,18 @@ public class UserDTO implements Serializable {
     public void setId(Long id) {this.id = id;}
     public Long getVersion() {return version;}
     public void setVersion(Long version) {this.version = version;}
+    public String getUserName() {return userName;}
+    public void setUserName(String userName) {this.userName = userName;}
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
     public String getFirstName() {return firstName;}
     public void setFirstName(String firstName) {this.firstName = firstName;}
     public String getLastName() {return lastName;}
     public void setLastName(String lastName) {this.lastName = lastName;}
-    public String getEmail() {return email;}
-    public void setEmail(String email) {this.email = email;}
-    public String getUserName() {return userName;}
-    public void setUserName(String userName) {this.userName = userName;}
+    public LocalDate getBirthDate() {return birthDate;}
+    public void setBirthDate(LocalDate birthDate) {this.birthDate = birthDate;}
+    public String getAvatar() {return avatar;}
+    public void setAvatar(String avatar) {this.avatar = avatar;}
     public String getPassword() {return password;}
     public void setPassword(String password) {this.password = password;}
     public List<RoleDTO> getRoles() {return roles;}
