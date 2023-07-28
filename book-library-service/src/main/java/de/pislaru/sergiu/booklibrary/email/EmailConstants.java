1e73fa3e4a1f118298b9c0f01d8c430a4ddac0cc
SergiuPs <sergiu.pislaru@web.de>
2023-07-28 21:48:46 +0200

    Adjusted registration-confirmation link

--- book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/email/EmailConstants.java
+++ book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/email/EmailConstants.java
@@ -2,7 +2,7 @@ package de.pislaru.sergiu.booklibrary.email;
 
 public final class EmailConstants {
     public final static String BASE_PACKAGE_PATH = "/templates";
-    public final static String CONFIRM_REGISTRATION_PATH = "/confirm-registration?token=";
+    public final static String CONFIRM_REGISTRATION_PATH = "/registration-confirmation?token=";
     public final static String PASSWORD_RESET_PATH = "/password-reset?token=";
 
     public final static String CONFIRMATION_LINK_TEMPLATE = "registration-confirmation-email.flth";
