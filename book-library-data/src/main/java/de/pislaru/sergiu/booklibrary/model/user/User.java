9624035ab0663e4d943fa103cfcf0a92c9101ca5
SergiuPs <sergiu.pislaru@web.de>
2023-07-28 21:53:25 +0200

    Added birthDate and avatar to User entity

--- book-library-data/src/main/java/de/pislaru/sergiu/booklibrary/model/user/User.java
+++ book-library-data/src/main/java/de/pislaru/sergiu/booklibrary/model/user/User.java
@@ -5,18 +5,13 @@ import de.pislaru.sergiu.booklibrary.model.address.Address;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
+import java.time.LocalDate;
 import java.util.*;
 
 @Entity
-
 public class User extends BaseEntity {
-
-    @Size(min = 2, max = 100)
-    private String firstName;
-
-    @Size(min = 2, max = 100)
-    private String lastName;
 
     @Size(min = 3, max = 50)
     private String userName;
@@ -24,12 +19,25 @@ public class User extends BaseEntity {
     @Email
     private String email;
 
+    @Size(min = 2, max = 100)
+    private String firstName;
+
+    @Size(min = 2, max = 100)
+    private String lastName;
+
+    @Past
+    private LocalDate birthDate;
+
+    @Size(min = 6, max = 255)
+    private String avatar;
+
     @Size(min = 8, max = 255)
     private String password;
 
     private boolean enabled;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user", targetEntity = Address.class, fetch = FetchType.EAGER)
+    @OrderBy
     private Set<Address> addresses = new HashSet<>();
 
     @ManyToMany(cascade = {
@@ -44,12 +52,14 @@ public class User extends BaseEntity {
     public User() {
     }
 
-    public User(String firstName, String lastName, String userName, String email, String password,
-                boolean enabled, Set<Address> addresses, Set<Role> roles) {
+    public User(String firstName, String lastName, String userName, String email, LocalDate birth_date, String avatar,
+                String password, boolean enabled, Set<Address> addresses, Set<Role> roles) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.userName = userName;
         this.email = email;
+        this.birthDate = birth_date;
+        this.avatar = avatar;
         this.password = password;
         this.enabled = enabled;
         this.addresses = addresses;
@@ -58,26 +68,32 @@ public class User extends BaseEntity {
 
     @Override
     public String toString() {
-        return "User { "
+        return "User {"
                 + super.toString()
                 + ", first-name=" + firstName
                 + ", last-name=" + lastName
                 + ", e-mail=" + email
+                + ", birt-date=" + birthDate
+                + ", avatar=" + avatar
                 + ", user-name=" + userName
                 + ", enabled=" + enabled
-                + "\n, addresses: " + addresses
-                + "\n, roles: " + roles
+                + "\n addresses: " + addresses
+                + "\n roles: " + roles
                 + "}";
     }
 
-    public String getFirstName() {return firstName;}
-    public void setFirstName(String firstName) {this.firstName = firstName;}
-    public String getLastName() {return lastName;}
-    public void setLastName(String lastName) {this.lastName = lastName;}
     public String getUserName() {return userName;}
     public void setUserName(String userName) {this.userName = userName;}
     public String getEmail() {return email;}
     public void setEmail(String email) {this.email = email;}
+    public String getFirstName() {return firstName;}
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+    public String getLastName() {return lastName;}
+    public void setLastName(String lastName) {this.lastName = lastName;}
+    public LocalDate getBirthDate() {return birthDate;}
+    public void setBirthDate(LocalDate birthDate) {this.birthDate = birthDate;}
+    public String getAvatar() {return avatar;}
+    public void setAvatar(String avatar) {this.avatar = avatar;}
     public String getPassword() {return password;}
     public void setPassword(String password) {this.password = password;}
     public boolean isEnabled() {return enabled;}
