92095404a17376ed065990ffd19ff4cc5bc51f23
SergiuPs <sergiu.pislaru@web.de>
2023-08-01 20:10:19 +0200

    Deleted bucket-name property from AwsProperties

--- book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/aws/AwsProperties.java
+++ book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/aws/AwsProperties.java
@@ -10,7 +10,6 @@ public class AwsProperties {
     private String accessKey;
     private String secretKey;
     private String s3RegionName;
-    private String s3BucketName;
     private Long presignedUrlExpiresAfterMs;
 
     public String getAccessKey() {return accessKey;}
@@ -19,8 +18,6 @@ public class AwsProperties {
     public void setSecretKey(String secretKey) {this.secretKey = secretKey;}
     public String getS3RegionName() {return s3RegionName;}
     public void setS3RegionName(String s3RegionName) {this.s3RegionName = s3RegionName;}
-    public String getS3BucketName() {return s3BucketName;}
-    public void setS3BucketName(String s3BucketName) {this.s3BucketName = s3BucketName;}
     public Long getPresignedUrlExpiresAfterMs() {return presignedUrlExpiresAfterMs;}
     public void setPresignedUrlExpiresAfterMs(Long presignedUrlExpiresAfterMs) {this.presignedUrlExpiresAfterMs = presignedUrlExpiresAfterMs;}
 }
