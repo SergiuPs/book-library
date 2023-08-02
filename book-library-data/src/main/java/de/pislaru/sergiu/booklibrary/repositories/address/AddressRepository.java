662278417cc153dfcbf9ceac7fc06dffc97eaa47
SergiuPs <sergiu.pislaru@web.de>
2023-08-02 17:29:22 +0200

    Moved AddressRepository to own package

--- book-library-data/src/main/java/de/pislaru/sergiu/booklibrary/repositories/address/AddressRepository.java
+++ book-library-data/src/main/java/de/pislaru/sergiu/booklibrary/repositories/address/AddressRepository.java
@@ -1,11 +1,10 @@
-package de.pislaru.sergiu.booklibrary.repositories;
+package de.pislaru.sergiu.booklibrary.repositories.address;
 
 import de.pislaru.sergiu.booklibrary.model.address.Address;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-;
 
 @Repository
 public interface AddressRepository extends CrudRepository<Address, Long> {
