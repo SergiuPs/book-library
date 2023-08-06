57c0ec998434e602adbdb5304c33087a64b5c80c
SergiuPs <sergiu.pislaru@web.de>
2023-08-06 14:10:47 +0200

    Moved config to security package

--- book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/security/config/ServiceSecurityConfig.java
+++ book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/security/config/ServiceSecurityConfig.java
@@ -4,12 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableMethodSecurity
 public class ServiceSecurityConfig {
 
     private final UserDetailsService userDetailsService;
