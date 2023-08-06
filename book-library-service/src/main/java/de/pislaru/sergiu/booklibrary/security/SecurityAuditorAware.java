607bf4d20ec27a3a290eeb66b58ae69ced490a53
SergiuPs <sergiu.pislaru@web.de>
2023-08-06 22:23:33 +0200

    Updated SecurityAuditorAware after modifying SecurityUserHolder

--- book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/security/SecurityAuditorAware.java
+++ book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/security/SecurityAuditorAware.java
@@ -1,14 +1,21 @@
 package de.pislaru.sergiu.booklibrary.security;
 
+import de.pislaru.sergiu.booklibrary.auth.SecurityUser;
+
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component
 public class SecurityAuditorAware implements AuditorAware<Long> {
+
     @Override
     public Optional<Long> getCurrentAuditor() {
-        return  SecurityUserHolder.getIdOfTheAuthenticatedUser();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = auth.getPrincipal();
+        return (principal instanceof SecurityUser) ? Optional.of(((SecurityUser) principal).getId()) : Optional.empty();
     }
 }
