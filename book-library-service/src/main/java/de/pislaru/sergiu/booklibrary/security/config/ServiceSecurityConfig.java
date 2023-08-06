2bb85161c9aa158bd8c0a0038d0101b2f01018a4
SergiuPs <sergiu.pislaru@web.de>
2023-08-06 22:12:18 +0200

    Added global method security config

--- book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/security/config/ServiceSecurityConfig.java
+++ book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/security/config/ServiceSecurityConfig.java
@@ -1,17 +1,15 @@
-package de.pislaru.sergiu.booklibrary.config;
+package de.pislaru.sergiu.booklibrary.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@EnableMethodSecurity
 public class ServiceSecurityConfig {
 
     private final UserDetailsService userDetailsService;
