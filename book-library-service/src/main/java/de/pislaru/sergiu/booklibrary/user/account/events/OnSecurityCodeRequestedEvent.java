23d74d6583308ba3a44b443199dc4ac76efa0f4c
SergiuPs <sergiu.pislaru@web.de>
2023-08-10 19:37:40 +0200

    Adjusted parameters on SecurityCodeRequestedEvent

--- book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/user/account/events/OnSecurityCodeRequestedEvent.java
+++ book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/user/account/events/OnSecurityCodeRequestedEvent.java
@@ -1,17 +1,5 @@
 package de.pislaru.sergiu.booklibrary.user.account.events;
 
-import de.pislaru.sergiu.booklibrary.model.user.User;
-import org.springframework.context.ApplicationEvent;
+public record OnSecurityCodeRequestedEvent(String email, String firstName) {
 
-public class OnSecurityCodeRequestedEvent extends ApplicationEvent {
-
-    private User user;
-
-    public OnSecurityCodeRequestedEvent(User user) {
-        super(user);
-        this.user = user;
-    }
-
-    public User getUser() {return user;}
-    public void setUser(User user) {this.user = user;}
 }
--- book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/user/account/events/SecurityCodeRequestedListener.java
+++ book-library-service/src/main/java/de/pislaru/sergiu/booklibrary/user/account/events/SecurityCodeRequestedListener.java
@@ -2,24 +2,21 @@ package de.pislaru.sergiu.booklibrary.user.account.events;
 
 import de.pislaru.sergiu.booklibrary.email.*;
 import de.pislaru.sergiu.booklibrary.email.service.EmailService;
-import de.pislaru.sergiu.booklibrary.model.user.User;
 import de.pislaru.sergiu.booklibrary.user.account.service.SecurityCodeService;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import javax.mail.MessagingException;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
-public class SecurityCodeRequestedListener implements ApplicationListener<OnSecurityCodeRequestedEvent> {
+public class SecurityCodeRequestedListener {
 
-    private static final Logger logger = LoggerFactory.getLogger(UserRegistrationListener.class);
+    private static final Logger logger = LoggerFactory.getLogger(SecurityCodeRequestedListener.class);
 
     private final EmailService emailService;
     private final SecurityCodeService securityCodeService;
@@ -29,19 +26,18 @@ public class SecurityCodeRequestedListener implements ApplicationListener<OnSecu
         this.securityCodeService = securityCodeService;
     }
 
-    @Override
-    public void onApplicationEvent(OnSecurityCodeRequestedEvent event) {
-        User user = event.getUser();
-        String securityCode = securityCodeService.createSecurityCode(user.getEmail());
+    @EventListener
+    public void sendSecurityCode(OnSecurityCodeRequestedEvent event) {
+        String securityCode = securityCodeService.createSecurityCode(event.email());
 
         Map<String, Object> templateModel = new HashMap<>();
         templateModel.put("securityCode", securityCode);
-        templateModel.put("firstName", user.getFirstName());
+        templateModel.put("firstName", event.firstName());
 
-        EmailContent emailContent = emailService.createEmailContent(EmailConstants.EMAIL_SECURITY_CODE_TEMPLATE,
+        EmailContent emailContent = emailService.createEmailContent(EmailConstants.SECURITY_CODE_TEMPLATE,
                                                                                     templateModel, securityCode);
 
-        Email email = new Email("sergiu.pislaru@web.de",
+        Email email = new Email(event.email(),
                 EmailSender.NO_REPLY,
                 EmailSubject.SECURITY_CODE,
                 emailContent);
